7ee18cf0abb964971987de7b8939ebaddc2ff450
marcinsudol <marcin.sudol@outlook.com>
2021-01-22 13:56:12 +0100

    added widewindow helper function

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,9 @@
-// -------------------------------------------------------
-// UTILITIES
-// -------------------------------------------------------
+const BREAKDOWN_MD = 800;
+
+const wideWindow = () => {
+  return window.innerWidth >= BREAKDOWN_MD;
+};
+
 function log(str) {
   console.log(str);
 }
