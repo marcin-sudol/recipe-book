11552cf2d91c4acc7af5eef047ae667d364ae947
marcinsudol <marcin.sudol@outlook.com>
2021-01-27 13:01:39 +0100

    updated message

--- src/js/message.js
+++ src/js/message.js
@@ -17,12 +17,17 @@ class Message {
       )
       .finished.then(() => {
         this.messageContainer.style.top = "1em";
-        this.messageContainer.style.opacity = "1";
         setTimeout(() => {
           this.messageContainer
-            .animate([{ opacity: "1" }, { opacity: "0" }], {
-              duration: 400,
-            })
+            .animate(
+              [
+                { right: "1em", opacity: "1" },
+                { right: "-10em", opacity: "0" },
+              ],
+              {
+                duration: 400,
+              }
+            )
             .finished.then(() => {
               this.messageContainer.style.display = "none";
               this.messageContainer.style.top = "-10em";
