f747fde79cac572c9a6dcfb730ae030fe78be1a8
marcinsudol <marcin.sudol@outlook.com>
2021-01-29 17:22:21 +0100

    message with role alert

--- src/js/message.js
+++ src/js/message.js
@@ -1,13 +1,28 @@
 class Message {
   constructor() {
-    this.messageContainer = document.getElementById("message-container");
-    this.messageTxt = document.getElementById("message-txt");
+    this.messageTemplate = document.getElementById("message-template");
+    this.nextId = 0;
+  }
+
+  getNextId() {
+    const nextId = this.nextId;
+    this.nextId++;
+    return nextId;
   }
 
   displayMessage(message) {
-    this.messageTxt.textContent = message;
-    this.messageContainer.style.display = "block";
-    this.messageContainer
+    const dataId = this.getNextId();
+    // creates document fragment;
+    const messageFragment = this.messageTemplate.content.cloneNode(true);
+    messageFragment.querySelector(".message-container").dataset.id = dataId;
+    messageFragment.querySelector(".message-txt").textContent = message;
+    // append fragment's content into dom
+    document.body.appendChild(messageFragment);
+    // find added element and animate it
+    const messageElement = document.querySelector(
+      `.message-container[data-id="${dataId}"]`
+    );
+    messageElement
       .animate(
         [
           { top: "-2em", opacity: "0" },
@@ -16,9 +31,8 @@ class Message {
         { duration: 400 }
       )
       .finished.then(() => {
-        this.messageContainer.style.top = "1em";
         setTimeout(() => {
-          this.messageContainer
+          messageElement
             .animate(
               [
                 { right: "1em", opacity: "1" },
@@ -29,8 +43,7 @@ class Message {
               }
             )
             .finished.then(() => {
-              this.messageContainer.style.display = "none";
-              this.messageContainer.style.top = "-10em";
+              document.body.removeChild(messageElement);
             });
         }, 1500);
       });
