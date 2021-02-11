6fd1aa893feb14b2a1d8f31282c13373f7b2a6f4
marcinsudol <marcin.sudol@outlook.com>
2021-02-11 15:32:46 +0100

    nav.js: rearranged, added comments

--- src/js/nav.js
+++ src/js/nav.js
@@ -6,6 +6,7 @@ class Nav {
     resetRecipesCallback,
     updateTabIndexCallback
   ) {
+    // Binding methods
     this.show = this.show.bind(this);
     this.hide = this.hide.bind(this);
     this.clickedAddRecipe = this.clickedAddRecipe.bind(this);
@@ -13,21 +14,32 @@ class Nav {
     this.clickedResetList = this.clickedResetList.bind(this);
     this.showTooltip = this.showTooltip.bind(this);
     this.hideTooltip = this.hideTooltip.bind(this);
+
+    // Setting DOM elements
     this.nav = document.getElementById("nav");
     this.navList = document.getElementById("nav-list");
     this.navMenuButton = document.getElementById("nav-menu-button");
     this.bgMenuButton = document.getElementById("bg-menu-button");
     this.recipeWindow = document.getElementById("recipe-window");
+
+    // Saving callbacks
     this.displayRecipeCallback = displayRecipeCallback;
     this.openEditorCallback = openEditorCallback;
     this.resetRecipesCallback = resetRecipesCallback;
     this.updateTabIndexCallback = updateTabIndexCallback;
+
+    // Adding events listeners
     this.navMenuButton.onclick = this.hide;
     document.getElementById("nav-add-button").onclick = this.clickedAddRecipe;
     document.getElementById("nav-reset-button").onclick = this.clickedResetList;
     this.addList(arr, "fade");
   }
 
+  // ----------------------------------------------------------------
+  // Managing nav's visibility
+  // ----------------------------------------------------------------
+
+  // Show nav
   show() {
     this.nav.classList.remove("hidden");
     this.recipeWindow.classList.add("narrower");
@@ -35,6 +47,7 @@ class Nav {
     this.navMenuButton.focus();
   }
 
+  // Hide nav
   hide() {
     this.nav.classList.add("hidden");
     this.recipeWindow.classList.remove("narrower");
@@ -42,39 +55,50 @@ class Nav {
     this.bgMenuButton.focus();
   }
 
-  setTabIndex(tabIndex) {
-    const buttons = this.nav.querySelectorAll("button");
-    buttons.forEach((button) => {
-      button.setAttribute("tabindex", tabIndex);
-    });
-  }
-
-  enableTab() {
-    this.setTabIndex("0");
-  }
-
-  disableTab() {
-    this.setTabIndex("-1");
-  }
-
+  // Check if nav is visible
   isVisible() {
     return !this.nav.classList.contains("hidden");
   }
 
-  getRating(obj) {
-    let rating;
-    if (obj.rating.votes > 0)
-      rating = (obj.rating.sum / obj.rating.votes).toFixed(1);
-    else rating = "-";
-    return rating;
+  // ----------------------------------------------------------------
+  // Managing list items
+  // ----------------------------------------------------------------
+
+  // Add items from array to nav list (without removing previous items)
+  addList(arr, styleIn) {
+    arr.forEach((obj) => this.addItem(obj));
+    if (styleIn === "fade") {
+      this.navList.animate(
+        {
+          opacity: [0, 1],
+        },
+        {
+          duration: 400,
+          easing: "ease-out",
+        }
+      );
+    }
   }
 
+  // Remove all items from nav list
+  clearList() {
+    while (this.navList.childElementCount > 0) {
+      this.navList.removeChild(this.navList.lastElementChild);
+    }
+  }
+
+  // Fill nav list with items from array (with removing previous items)
+  updateList(arr, styleIn) {
+    this.clearList();
+    this.addList(arr, styleIn);
+  }
+
+  // Add single item to nav list
   addItem(obj) {
     const item = document.createElement("li");
     item.className = "nav-item";
     item.id = "nav-item-" + obj.id;
     item.dataset.id = obj.id;
-
     item.innerHTML = `<button class="nav-item-button" type="button">${
       obj.name
     }</button>
@@ -85,11 +109,11 @@ class Nav {
     this.navList.appendChild(item);
 
     item.querySelector(".nav-item-button").onclick = this.clickedDisplayRecipe;
-
     item.querySelector(".nav-item-rating").onmouseenter = this.showTooltip;
     item.querySelector(".nav-item-rating").onmouseleave = this.hideTooltip;
   }
 
+  // Update item's name and rating on nav list
   updateItem(obj) {
     const item = document.getElementById("nav-item-" + obj.id);
     item.querySelector(".nav-item-button").textContent = obj.name;
@@ -98,6 +122,7 @@ class Nav {
       obj.rating.votes + " votes";
   }
 
+  // Remove single item from nav list
   removeItem(obj) {
     const item = document.getElementById("nav-item-" + obj.id);
     item
@@ -114,45 +139,49 @@ class Nav {
       });
   }
 
-  addList(arr, styleIn) {
-    arr.forEach((obj) => this.addItem(obj));
-    if (styleIn === "fade") {
-      this.navList.animate(
-        {
-          opacity: [0, 1],
-        },
-        {
-          duration: 400,
-          easing: "ease-out",
-        }
-      );
-    }
+  // ----------------------------------------------------------------
+  // Managing tab interactions
+  // ----------------------------------------------------------------
+
+  // Enable interaction with tab key
+  enableTab() {
+    this.setTabIndex("0");
   }
 
-  clearList() {
-    while (this.navList.childElementCount > 0) {
-      this.navList.removeChild(this.navList.lastElementChild);
-    }
+  // Disable interaction with tab key
+  disableTab() {
+    this.setTabIndex("-1");
   }
 
-  updateList(arr, styleIn) {
-    this.clearList();
-    this.addList(arr, styleIn);
+  // Set tabindex for all interactive elements
+  setTabIndex(tabIndex) {
+    const buttons = this.nav.querySelectorAll("button");
+    buttons.forEach((button) => {
+      button.setAttribute("tabindex", tabIndex);
+    });
   }
 
+  // ----------------------------------------------------------------
+  // Event handlers
+  // ----------------------------------------------------------------
+
+  // Clicked add new recipe
   clickedAddRecipe() {
     this.openEditorCallback();
   }
 
+  // Clicked recipe on a list
   clickedDisplayRecipe(event) {
     const id = parseInt(event.target.parentElement.dataset.id);
     this.displayRecipeCallback(id);
   }
 
+  // Clicked reset list
   clickedResetList() {
     this.resetRecipesCallback();
   }
 
+  // When mouse enters rating
   showTooltip(event) {
     const tooltip = event.target.querySelector(".rating-tooltip");
     const targetRect = event.target.getBoundingClientRect();
@@ -161,8 +190,22 @@ class Nav {
     tooltip.style.top = targetRect.bottom - 25 + "px";
   }
 
+  // When mouse leaves rating
   hideTooltip(event) {
     const tooltip = event.target.querySelector(".rating-tooltip");
     tooltip.classList.remove("visible");
+  }
+
+  // ----------------------------------------------------------------
+  // Helper functions
+  // ----------------------------------------------------------------
+
+  // Return recipe's rating in format for displaying
+  getRating(obj) {
+    let rating;
+    if (obj.rating.votes > 0)
+      rating = (obj.rating.sum / obj.rating.votes).toFixed(1);
+    else rating = "-";
+    return rating;
   }
 }
